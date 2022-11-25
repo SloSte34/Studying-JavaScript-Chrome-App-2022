43c4defec7f9ff2ee6e111dbb2ad1d3f52652acd
minsu <cocacolazero024@gmail.com>
2022-11-26 01:40:36 +0900

    Modify API name

--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,9 @@
-const API_KEY = config.apiKey;
-console.log(API_KEY);
+const WEATHER_API_KEY = config.apiKey;
 
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`;
   fetch(weatherUrl)
     .then((response) => response.json())
     .then((data) => {
