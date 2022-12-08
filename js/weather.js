e939730273b40d2cc7a0be4c223af472ed37c03f
minsu <cocacolazero024@gmail.com>
2022-12-08 23:14:52 +0900

    Delete WeatherURL

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,7 @@
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`;
+  // const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=c05dfc7728a8a86f021ca2026c300633&units=metric`;
   fetch(weatherUrl)
     .then((response) => response.json())
     .then((data) => {
